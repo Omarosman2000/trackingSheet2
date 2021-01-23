960c1f2a1aa68ef11cb79e921fe733b24993a17a
sarahlarusso <sjlarusso@wpi.edu>
2021-01-23 14:14:13 -0500

    main

--- src/net/datastructures/Main.java
+++ src/net/datastructures/Main.java
@@ -10,17 +10,8 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
 
-        
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter course name: ");
-        String courseName =input.nextLine();
-        System.out.println(courseName);
-
-        if(courseName == null){
-            System.out.println("Invalid course name! ");
-        }
-
+    	Controller.initialize();
+    	Controller.returnRecommendations();
 
     }
 }
