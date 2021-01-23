f52a6c5d0912a46b31bb331de077464f441b27d6
ngrumski <grumski88@gmail.com>
2021-01-22 21:43:36 -0500

    add course parser

---
+++ src/net/datastructures/CourseParser.java
@@ -0,0 +1,12 @@
+package net.datastructures;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import net.datastructures.Course;
+
+public class CourseParser {
+    public  HashMap<String,Course> parseFile(String fileName){
+
+        return null;
+    }
+}
