602df1cf6dce8722fc8e86a698e6747a62f67c68
Omar1 <omarali.2000.16@gmail.com>
2021-01-23 01:14:18 -0500

    user input

--- src/net/datastructures/Main.java
+++ src/net/datastructures/Main.java
@@ -1,9 +1,21 @@
 package net.datastructures;
 
+
+import java.util.Scanner;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter course name: ");
+        String courseName =input.nextLine();
+        System.out.println(courseName);
+
+            if(courseName == null){
+                System.out.println("Invalid course name! ");
+            }
 
 
     }
